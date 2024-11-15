f8cb42df4d28517a2c0d81e832b5a6407105168e
Eric Afes <eriicafes@gmail.com>
2024-11-15 17:08:47 +0100

    Add layout to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,29 @@
+import { AuthProvider } from "@/context/auth";
+import { CartProvider } from "@/context/cart";
 import Link from "next/link";
+import { Footer } from "./(shop)/footer";
+import { Header } from "./(shop)/header";
 
 export default function NotFound() {
   return (
-    <main>
-      <div className="container py-20">
-        <h1 className="text-3xl font-bold mb-6 capitalize text-center">
-          Page not found
-        </h1>
-        <p className="text-center text-gray-600">
-          The link you followed does not exist.{" "}
-          <Link href="/" className="underline">
-            Go home
-          </Link>
-        </p>
-      </div>
-    </main>
+    <AuthProvider>
+      <CartProvider>
+        <Header />
+        <main>
+          <div className="container py-20">
+            <h1 className="text-3xl font-bold mb-6 capitalize text-center">
+              Page not found
+            </h1>
+            <p className="text-center text-gray-600">
+              The link you followed does not exist.{" "}
+              <Link href="/" className="underline">
+                Go to shop
+              </Link>
+            </p>
+          </div>
+        </main>
+        <Footer />
+      </CartProvider>
+    </AuthProvider>
   );
 }
